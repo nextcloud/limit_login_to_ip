b65e0ee6849c57bb6c6e617b4f7b5d8266ee62f6
Benjamin Gaussorgues <benjamin.gaussorgues@nextcloud.com>
2023-08-07 12:22:07 +0200

    Move from app.php to Application.php
    Use new `BeforeUserLoggedInEvent` instead of `pre_login` hook
    
    Signed-off-by: Benjamin Gaussorgues <benjamin.gaussorgues@nextcloud.com>

--- tests/unit/IsRequestAllowedTest.php
+++ tests/unit/IsRequestAllowedTest.php
@@ -24,41 +24,36 @@ declare(strict_types=1);
 
 namespace OCA\LimitLoginToIp\Tests\Unit;
 
-use OCA\LimitLoginToIp\LoginHookListener;
+use OCA\LimitLoginToIp\IsRequestAllowed;
 use OCP\IConfig;
 use OCP\IRequest;
-use OCP\IURLGenerator;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
-class LoginHookListenerTest extends TestCase {
+class IsRequestAllowedTest extends TestCase {
 	/** @var IConfig|MockObject */
 	private $config;
 	/** @var IRequest|MockObject */
 	private $request;
-	/** @var IURLGenerator|MockObject */
-	private $urlGenerator;
-	private LoginHookListener $listener;
+	private $lastHttpCode;
+	private IsRequestAllowed $isRequestAllowed;
 
 	protected function setUp(): void {
 		parent::setUp();
 
 		$this->config = $this->createMock(IConfig::class);
 		$this->request = $this->createMock(IRequest::class);
-		$this->urlGenerator = $this->createMock(IURLGenerator::class);
 		
-		$this->listener = new LoginHookListener(
+		$this->isRequestAllowed = new IsRequestAllowed(
 			$this->config,
 			$this->request,
-			$this->urlGenerator,
-			true,
 		);
 	}
 
 	/**
 	 * @dataProvider ipSubnetProvider
 	 */
-	public function testMatchRange(string $ipAddress, ?string $subnet, bool $shouldBeTrue): void {
+	public function testMatchRange(string $ipAddress, ?string $subnet, bool $shouldSucceed): void {
 		$this->config->expects($this->once())
 			->method('getAppValue')
 			->with('limit_login_to_ip', 'whitelisted.ranges', '')
@@ -69,20 +64,26 @@ class LoginHookListenerTest extends TestCase {
 				->method('getRemoteAddress')
 				->willReturn($ipAddress);
 		}
-		$shouldBeTrue
-			? $this->assertTrue($this->listener->isLoginAllowed())
-			: $this->assertFalse($this->listener->isLoginAllowed());
 
+		$result = ($this->isRequestAllowed)();
+		$shouldSucceed
+			? $this->assertTrue($result)
+			: $this->assertFalse($result);
 	}
 
+	/**
+	 * @return mixed[]
+	 */
 	public function ipSubnetProvider(): array {
 		return [
 			['1.2.3.4', '', true],
 			['1.2.3.4', '1.2.3.4', true],
+			['1.2.3.4', '1.2.3.4/', true],
 			['1.2.3.4', '1.2.3.4/32', true],
 			['1.2.3.5', '1.2.3.4/32', false],
 			['10.0.3.5', '10.0.0.0/16', true],
 			['85d2:057d:e774:c8f7:5071:0fe0:6deb:62bd', '', true],
+			['85d2:057d:e774:c8f7:5071:0fe0:6deb:62bd', '85d2:057d:e774:c8f7::', false],
 			['85d2:057d:e774:c8f7:5071:0fe0:6deb:62bd', '85d2:057d:e774:c8f7::/64', true],
 			['85d2:057d:e774:c8f7:5071:0fe0:6deb:62be', '85d2:057d:e774:c8f7:5071:0fe0:6deb:62bd', false],
 		];
