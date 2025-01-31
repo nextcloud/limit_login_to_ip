e693ce4f5bb3fcdbbb96b24448bf976987390ab4
Benjamin Gaussorgues <benjamin.gaussorgues@nextcloud.com>
2025-01-31 11:41:42 +0100

    chore: add rector with Nextcloud rules
    Signed-off-by: Benjamin Gaussorgues <benjamin.gaussorgues@nextcloud.com>

---
+++ rector.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+use Nextcloud\Rector\Set\NextcloudSets;
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+	->withPaths([
+		__DIR__ . '/appinfo',
+		__DIR__ . '/lib',
+		__DIR__ . '/templates',
+		__DIR__ . '/tests',
+	])
+	->withPhpSets(php81: true)
+	->withTypeCoverageLevel(0)
+	->withImportNames(importShortClasses:false)
+	->withPreparedSets(
+		codeQuality: true,
+		codingStyle: true,
+		deadCode: true,
+		earlyReturn: true,
+		instanceOf: true,
+		privatization: true,
+		strictBooleans: true,
+	)
+	->withSets([
+		NextcloudSets::NEXTCLOUD_27,
+	]);
